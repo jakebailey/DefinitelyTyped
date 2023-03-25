18520d9505c749184ffc2d8e1dae9f6d524ea97b
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-03-24 19:37:46 -0700

    Update script 3

--- scripts/create-package-jsons.js
+++ scripts/create-package-jsons.js
@@ -3,6 +3,8 @@ import path from "path";
 import url from "url";
 import fetch from "node-fetch";
 import assert from "assert";
+import PQueue from 'p-queue';
+import ora from 'ora';
 
 const __filename = url.fileURLToPath(new URL(import.meta.url));
 const __dirname = path.dirname(__filename);
@@ -41,13 +43,25 @@ function compareComparableValues(a, b) {
         1;
 }
 
+const desiredPropertyOrder = [
+    "private",
+    "name",
+    "version",
+    "license",
+    "type",
+    "exports",
+    "types",
+    "typesVersions",
+    "dependencies",
+    "devDependencies",
+]
+
 /**
  * @param {string} pkgName
  * @param {string} p
  * @param {string} [version]
  */
 async function handlePackage(pkgName, p, version) {
-    console.log(pkgName);
     const packageJsonPath = path.join(p, "package.json");
     /** @type {Record<string, any>} */
     const packageJsonContents = { name: pkgName, private: true };
@@ -68,36 +82,40 @@ async function handlePackage(pkgName, p, version) {
     const url = version ? `https://unpkg.com/${pkgName}@${version}/package.json` : `https://registry.npmjs.org/${pkgName}/latest`
     const response = await fetch(url);
     const publishedPackageJson = await response.json();
-    packageJsonContents.dependencies ||= {};
+    packageJsonContents.dependencies = {};
 
-    for (const key of Object.keys(packageJsonContents.dependencies)) {
-        if (allPackages.has(key)) {
-            console.log(`Check ${prettyPath(p)}/package.json for ${key} dependency, may need to be mapped to a folder.`)
+    // for (const key of Object.keys(packageJsonContents.dependencies)) {
+    //     if (allPackages.has(key)) {
+    //         console.log(`Check ${prettyPath(p)}/package.json for ${key} dependency, may need to be mapped to a folder.`)
+    //     }
+    // }
+
+    for (const [key, value] of Object.entries(publishedPackageJson.dependencies || {})) {
+        if (key === pkgName) {
+            // Weird self dependency.
+            continue;
         }
-    }
+        packageJsonContents.dependencies[key] = value;
 
-    publishedPackageJson.dependencies ||= {};
+        // if (!packageJsonContents.dependencies[key]) {
+        //     // Dependency added by the publisher, map locally.
+        //     const depRoot = path.join(types, key.slice("@types/".length));
+        //     if (!fs.existsSync(depRoot)) {
+        //         // Somehow, we got an unlisted * dep?
+        //         console.log(`Check ${prettyPath(p)}/package.json; unlisted dep ${key}`)
+        //         packageJsonContents.dependencies[key] = "*";
+        //     }
 
-    for (const [key, value] of Object.entries(publishedPackageJson.dependencies)) {
-        if (!packageJsonContents.dependencies[key]) {
-            // Dependency added by the publisher, map locally.
-            const depRoot = path.join(types, key.slice("@types/".length));
-            if (!fs.existsSync(depRoot)) {
-                // Somehow, we got an unlisted * dep?
-                console.log(`Check ${prettyPath(p)}/package.json; unlisted dep ${key}`)
-                packageJsonContents.dependencies[key] = "*";
-            }
+        //     const depRelative = path.relative(p, depRoot);
+        //     if (!depRelative) {
+        //         // Some packages appear to accidentally depend on themselves?
+        //         console.log(`Check ${prettyPath(p)}/package.json; depends on itself`)
+        //         continue;
+        //     }
 
-            const depRelative = path.relative(p, depRoot);
-            if (!depRelative) {
-                // Some packages appear to accidentally depend on themselves?
-                console.log(`Check ${prettyPath(p)}/package.json; depends on itself`)
-                continue;
-            }
-
-            const depVersion = value === "*" ? "*" : `^${value.slice(1)}`;
-            packageJsonContents.dependencies[key] = `workspace:${depVersion}`;
-        }
+        //     const depVersion = value === "*" ? "*" : `^${value.slice(1)}`;
+        //     packageJsonContents.dependencies[key] = `workspace:${depVersion}`;
+        // }
     }
 
     if (Object.keys(packageJsonContents.dependencies).length === 0) {
@@ -107,15 +125,43 @@ async function handlePackage(pkgName, p, version) {
         packageJsonContents.dependencies = Object.fromEntries(Object.entries(packageJsonContents.dependencies).sort((a, b) => compareComparableValues(a[0], b[0])))
     }
 
+    // TODO(jakebailey): put after name and private
     const splitVersion = publishedPackageJson.version.split(".")
-    splitVersion[splitVersion.length-1] = "99999";
+    splitVersion[splitVersion.length-1] = "0";
     packageJsonContents.version = splitVersion.join(".");
 
-    await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageJsonContents, undefined, 4) + "\n");
+    /** @type {Record<string, unknown>} */
+    const result = {};
+
+    for (const prop of desiredPropertyOrder) {
+        result[prop] = packageJsonContents[prop];
+        delete packageJsonContents[prop];
+    }
+
+    assert.deepStrictEqual(packageJsonContents, {}, `${prettyPath(p)}`);
+
+    Object.assign(result, packageJsonContents)
+
+    await fs.promises.writeFile(packageJsonPath, JSON.stringify(result, undefined, 4) + "\n");
 }
 
-async function main() {
-    for (const name of packages) {
+let progress = 0;
+const total = packages.length;
+
+const spinner = ora('').start();
+
+/**
+ * @param {string} pkgName
+ */
+function done(pkgName) {
+    progress++;
+    spinner.text = `${progress}/${total} ${pkgName}`;
+}
+
+const queue = new PQueue({ concurrency: 8 });
+
+for (const name of packages) {
+    queue.add(async () => {
         const p = path.join(types, name)
         const pkgName = npmPackageName(name);
         await handlePackage(pkgName, p);
@@ -127,7 +173,8 @@ async function main() {
                 await handlePackage(pkgName, subP, subdir);
             }
         }
-    }
+        done(pkgName);
+    })
 }
 
-main();
+queue.onEmpty().then(() => process.exit(0));
